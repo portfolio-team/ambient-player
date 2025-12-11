f6068784262fa4b7ec6f4e05f09cf102e303e212
portfolio-team <yamamoto.kotaro.portfolio@gmail.com>
2025-12-11 11:59:05 +0900

    update: build config for SSG

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  output: 'export',
+  trailingSlash: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+}
 
 export default nextConfig;
